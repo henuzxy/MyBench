07045d261f74f016331c741d1bed51b19b66f53d
zhaoxiaoyu <1961714414@qq.com>
2020-05-08 01:34:03 +0800

    feat:temp

--- CliEngine.h
+++ CliEngine.h
@@ -1,11 +1,9 @@
 #include <string>
 #include "InputInfo.h"
-#include "common/SingleTon.h"
 namespace CLI{
 
-class CliEngine: public SingleTon<CliEngine>{
+class CliEngine{
 public:
-    friend SingleTon;
     static void Usage();
     static InputInfo GetInput(int argc,char *argv[]);
 };
--- test.cpp
+++ test.cpp
@@ -2,7 +2,7 @@
 #include "CliEngine.h"
 using namespace std;
 int main(int argc, char* argv[]){
-    CLI::CliEngine::getinstance().Usage();
-    CLI::CliEngine::getinstance().GetInput(argc,argv);
+    CLI::CliEngine::Usage();
+    CLI::CliEngine::GetInput(argc,argv);
     return 0;
 }
