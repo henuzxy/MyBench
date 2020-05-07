947b6535d0072ef0f0155d6ca1711c0a99f308f6
zhaoxiaoyu <1961714414@qq.com>
2020-05-08 01:29:19 +0800

    feat: tests

--- test.cpp
+++ test.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include "CliEngine.h"
 using namespace std;
-int main(int argc,char *argv[]){
-    CLI::CliEngine::GetInput(argc,argv);
+int main(int argc, char* argv[]){
+    CLI::CliEngine::getinstance().Usage();
+    CLI::CliEngine::getinstance().GetInput(argc,argv);
     return 0;
 }
